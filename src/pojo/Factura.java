4af3dbc74496db149bde5f82c8d602fdf2cf007d
Benjahoff <oriozabalabenja@gmail.com>
2021-08-30 19:31:06 -0300

    second commit}

--- src/pojo/Factura.java
+++ src/pojo/Factura.java
@@ -1,5 +1,25 @@
 package pojo;
 
 public class Factura {
+	private int idFactura;
+	private int idCliente;
+	
+	public Factura() {
+		super();	
+	}
+	
+	public Factura(int idFactura, int idCliente) {
+		super();
+		this.idFactura = idFactura;
+		this.idCliente = idCliente;
+	}
 
+	public int getIdFactura() {
+		return idFactura;
+	}
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+	
 }
